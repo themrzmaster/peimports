a5e6567465ea18c0175fd00dc8488638114c909c
Lucca Zenobio <themrzmaster@gmail.com>
2018-02-20 09:18:00 -0300

    r

---
+++ files/test.py
@@ -0,0 +1,17 @@
+for file in os.listdir(directory):
+	if not file.endswith(".py"):
+		pe = pefile.PE(file)
+		ep = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+		ep_ava = ep+pe.OPTIONAL_HEADER.ImageBase
+		data = pe.get_memory_mapped_image()
+		offset = 0
+		while offset < len(data):
+			try:
+				i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
+				line = pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)
+				if '0x420450' in line:
+					print line, offset
+				offset += i.length
+			except TypeError as et:
+				#print 'erro ', offset
+				offset += 12
