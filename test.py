ac6a02a2623c9e224d3716d22ceae845e6cc8800
themrzmaster <themrzmaster@gmail.com>
2018-02-20 11:29:56 -0300

    s

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ def extract(file):
 		module_name = entry.dll
 		for imp in entry.imports: #for each function imported calculate how many times it was called, jumped
 			lista[str(hex(imp.address))] = imp.name
-	lista_offset = tuple(list(lista.keys()))
+	lista_offset = list(lista.keys())
 	print lista_offset
 	offset = 0
 	count = 0
