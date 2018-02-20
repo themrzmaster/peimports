ffc80a03c3f314a055e1d3579571105743583529
themrzmaster <themrzmaster@gmail.com>
2018-02-20 14:21:28 -0300

    try catcth

--- test.py
+++ test.py
@@ -41,40 +41,47 @@ def updateModules(file):
 	print file
 	hash_file = between(file, "_", ".vir")
 	pe = pefile.PE(file)
-	for entry in pe.DIRECTORY_ENTRY_IMPORT:
-		name = entry.dll
-		query_get_module = "SELECT * FROM modules WHERE name = "
-		query_get = query_get_module + "'"+name+"'"
-		#print query_get
-		cursor.execute(query_get)
-		data = cursor.fetchall()
-		if not data:
-			#not added yet
-			query_add_module = "INSERT INTO modules (name) VALUES ('" + name + "')"
-			#print query_add_module
-			cursor.execute(query_add_module)
-			emp_no = cursor.lastrowid
-			#print emp_no
-		cnx.commit()
+	try:
+		for entry in pe.DIRECTORY_ENTRY_IMPORT:
+			name = entry.dll
+			query_get_module = "SELECT * FROM modules WHERE name = "
+			query_get = query_get_module + "'"+name+"'"
+			#print query_get
+			cursor.execute(query_get)
+			data = cursor.fetchall()
+			if not data:
+				#not added yet
+				query_add_module = "INSERT INTO modules (name) VALUES ('" + name + "')"
+				#print query_add_module
+				cursor.execute(query_add_module)
+				emp_no = cursor.lastrowid
+				#print emp_no
+			cnx.commit()
+	except AttributeError as e:
+		#skip
+		print e		
 
 def updateAPI(file):
 	pe = pefile.PE(file)
-	for entry in pe.DIRECTORY_ENTRY_IMPORT:
-		name = entry.dll
-		query_get_id = "SELECT idmodules FROM modules WHERE name = '" + name + "'"
-		cursor.execute(query_get_id)
-		row = cursor.fetchall()
-		mod_id = row[0][0]
-		for imp in entry.imports:
-			if imp.name:
-				query_get = "SELECT * FROM apis WHERE name = '" + imp.name + "'"
-				cursor.execute(query_get)
-				data = cursor.fetchall()
-				if not data:
-					m_id = str(mod_id)
-					query_set = "INSERT INTO apis (module, name) VALUES (" + m_id + ",'" + imp.name + "')" 
-					cursor.execute(query_set)
-		cnx.commit()
+	try:
+		for entry in pe.DIRECTORY_ENTRY_IMPORT:
+			name = entry.dll
+			query_get_id = "SELECT idmodules FROM modules WHERE name = '" + name + "'"
+			cursor.execute(query_get_id)
+			row = cursor.fetchall()
+			mod_id = row[0][0]
+			for imp in entry.imports:
+				if imp.name:
+					query_get = "SELECT * FROM apis WHERE name = '" + imp.name + "'"
+					cursor.execute(query_get)
+					data = cursor.fetchall()
+					if not data:
+						m_id = str(mod_id)
+						query_set = "INSERT INTO apis (module, name) VALUES (" + m_id + ",'" + imp.name + "')" 
+						cursor.execute(query_set)
+			cnx.commit()
+		except AttributeError as e:
+			print e	
 
 def extract(file):
 	lista = {}
@@ -87,56 +94,59 @@ def extract(file):
 	ep = pe.OPTIONAL_HEADER.AddressOfEntryPoint
 	ep_ava = ep+pe.OPTIONAL_HEADER.ImageBase
 	data = pe.get_memory_mapped_image()
-	for entry in pe.DIRECTORY_ENTRY_IMPORT:
-		module_name = entry.dll
-		for imp in entry.imports: #for each function imported calculate how many times it was called, jumped
-			if imp.name:
-				lista[str(hex(imp.address))] = imp.name
-	lista_offset = list(lista.keys())
-	offset = 0
-	count = 0
-	for item in lista_offset:
-		lista_count[item] = 0
+	try:
+		for entry in pe.DIRECTORY_ENTRY_IMPORT:
+			module_name = entry.dll
+			for imp in entry.imports: #for each function imported calculate how many times it was called, jumped
+				if imp.name:
+					lista[str(hex(imp.address))] = imp.name
+		lista_offset = list(lista.keys())
+		offset = 0
+		count = 0
+		for item in lista_offset:
+			lista_count[item] = 0
 
-	#print lista_count	
-	while offset < len(data):
-		try:
-			i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
-			line = pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)
-			if line:
-				#if any(item in line for item in lista_offset):
-				for w in lista_offset:
-					if w in line:
-						lista_count[w] += 1
-					#count += 1
-				offset += i.length
-			else:
+		#print lista_count	
+		while offset < len(data):
+			try:
+				i = pydasm.get_instruction(data[offset:], pydasm.MODE_32)
+				line = pydasm.get_instruction_string(i, pydasm.FORMAT_INTEL, ep_ava+offset)
+				if line:
+					#if any(item in line for item in lista_offset):
+					for w in lista_offset:
+						if w in line:
+							lista_count[w] += 1
+						#count += 1
+					offset += i.length
+				else:
+					offset += 12
+			except TypeError as et:
+				#print 'erro ', offset
 				offset += 12
-		except TypeError as et:
-			#print 'erro ', offset
-			offset += 12
-		#lista_count[value] = count
-	#print lista_count
-	for keys, value in lista_count.iteritems():
-		name = lista[keys]
-		#print name
-		query_get_api = "SELECT idapis FROM apis WHERE name = '" + name + "'"
-		cursor.execute(query_get_api)
-		row = cursor.fetchall()
-		api_id = row[0][0]
+			#lista_count[value] = count
+		#print lista_count
+		for keys, value in lista_count.iteritems():
+			name = lista[keys]
+			#print name
+			query_get_api = "SELECT idapis FROM apis WHERE name = '" + name + "'"
+			cursor.execute(query_get_api)
+			row = cursor.fetchall()
+			api_id = row[0][0]
 
-		query_get_hash = "SELECT idfile FROM files WHERE hash = '" + str(hash_file) + "'"
-		cursor.execute(query_get_hash)
-		row = cursor.fetchall()
-		hash_id = row[0][0]
-		#check if already added
-		query_check = "SELECT * FROM extract WHERE hash = '" + str(hash_id) + "' AND api = '" + str(api_id) + "'"
-		cursor.execute(query_check)
-		data = cursor.fetchall()
-		if not data:
-			query_add = "INSERT INTO extract (hash, api, call_number) VALUES ('" + str(hash_id) + "', "  + str(api_id) + ", "  + str(value) + " )"
-			cursor.execute(query_add)
-		#print query_check
+			query_get_hash = "SELECT idfile FROM files WHERE hash = '" + str(hash_file) + "'"
+			cursor.execute(query_get_hash)
+			row = cursor.fetchall()
+			hash_id = row[0][0]
+			#check if already added
+			query_check = "SELECT * FROM extract WHERE hash = '" + str(hash_id) + "' AND api = '" + str(api_id) + "'"
+			cursor.execute(query_check)
+			data = cursor.fetchall()
+			if not data:
+				query_add = "INSERT INTO extract (hash, api, call_number) VALUES ('" + str(hash_id) + "', "  + str(api_id) + ", "  + str(value) + " )"
+				cursor.execute(query_add)
+			#print query_check
+	except AttributeError as e:
+		print e		
 	cnx.commit()	
 	#print lista
 
